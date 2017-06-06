c9f2de2abe249a83c84a1b6844d8ea9437b3a645
David Brodie <david.brodie122@gmail.com>
2017-06-05 17:55:32 -0700

    Add files for components.  Connect ControlButtons to App

---
+++ src/components/ControlButtons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ControlButtons = (props) => {
+  return (
+    <div>
+      <button>Price</button>
+      <button>Beds</button>
+      <button>Sq. ft.</button>
+      </div>
+  )
+};
+
+export default ControlButtons;

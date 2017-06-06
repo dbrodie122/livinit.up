17bcc6a8ef00b4a8bacade498babee3ec5833d8a
David Brodie <david.brodie122@gmail.com>
2017-06-05 17:58:19 -0700

    Connect Header component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import ControlButtons from './components/ControlButtons';
 
 class App extends Component {
   render() {
     return (
       <div>
-        It works
+        <Header />
+        <ControlButtons />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>Awesome Listings Widget</h1>
+    </div>
+  )
+};
+
+export default Header;

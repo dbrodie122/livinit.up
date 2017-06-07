6cbff09c1d7c8267a0c9c8ae416650926dd69eed
David Brodie <david.brodie122@gmail.com>
2017-06-07 09:03:04 -0700

    Change data to allow export/import statements

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import ControlButtons from './components/ControlButtons';
 import Listings from './components/Listings';
+import {__BATMAN_DATA__} from './data/batmanData';
+import {__SUPERMAN_DATA__} from './data/supermanData';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
+  //function to fetch data from the server apis
+    // formatData
+    // dispatch action to set it in state
   render() {
+    
     return (
       <div>
         <Header />

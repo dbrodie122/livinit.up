30a330be50bc237eb5300c18e83588b3448674f1
David Brodie <david.brodie122@gmail.com>
2017-06-11 22:57:49 -0700

    Make tiles clickable links. Update CSS

--- src/components/ListingTile.js
+++ src/components/ListingTile.js
@@ -4,7 +4,7 @@ const ListingTile = (props) => {
   const listing = props.listing;
   return (
     <div className="listing-box">
-      <div className="listing-inner-box">
+        <a href={listing.url} ><span className="tile"></span></a>
         <img src={listing.pic} alt={`Listing at ${listing.street}`}></img>
         <div className="built" style={listing.built ? {} : {visibility: 'hidden'}}>{'Built in'} {listing.built}</div>
         <p className="truncate address">{listing.street}</p>
@@ -15,7 +15,6 @@ const ListingTile = (props) => {
           <span> {listing.baths} {listing.baths <= 1 ? 'bath' : 'baths'} </span>&bull;
           <span> {listing.sqft ? `${listing.sqft} sq ft` : 'inquire'}</span>
         </div>
-      </div>
     </div>
   )
 };
